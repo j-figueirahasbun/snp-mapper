576f1d2aff9c6e80d881ae2316e78070299f3192
j-figueirahasbun <j.figueirahasbun@gmail.com>
2024-08-01 15:45:00 +0200

    Functional mapping and refactoring implemented.

--- api/src/main/kotlin/com/genes/snp_mapper/controller/SNPController.kt
+++ api/src/main/kotlin/com/genes/snp_mapper/controller/SNPController.kt
@@ -1,5 +1,7 @@
 package com.genes.snp_mapper.controller
 
+import com.genes.snp_mapper.Gene
+import com.genes.snp_mapper.GeneService
 import com.genes.snp_mapper.SNP
 import com.genes.snp_mapper.SNPService
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,10 +11,20 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/snp")
-class SNPController (private val snpService: SNPService){
+class SNPController (private val snpService: SNPService, private val geneService: GeneService){
 
     @GetMapping("/info/{snp}")
     fun getSNPInformation(@PathVariable snp:String): SNP {
         return snpService.obtainSNPInformation(snp)
+    }
+
+    @GetMapping("/mapping/info/{snp}")
+    fun positionalMappingSNP(@PathVariable snp:String): Gene {
+        return geneService.positionalMapping(snp)
+    }
+
+    @GetMapping("/info/{snp}")
+    fun functionalMappingSNP(@PathVariable snp:String): List<Gene> {
+        return geneService.functionalMapping(snp)
     }
 }
