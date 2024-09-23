760f026df5581045a0df0659c9b4f639000d64c2
j-figueirahasbun <j.figueirahasbun@gmail.com>
2024-09-23 14:52:10 +0200

    Implement unit tests for RankingService class

--- service/src/test/kotlin/com.genes.snp_mapper/RankingServiceTest.kt
+++ service/src/test/kotlin/com.genes.snp_mapper/RankingServiceTest.kt
@@ -12,9 +12,9 @@ class RankingServiceTest {
     private lateinit var geneService:GeneService
     private lateinit var rankingService: RankingService
 
-    private val positionalGene = "Gene1"
-    private val functionalGene = "Gene2"
-    private val eQTLGene = "Gene3"
+    private var positionalGene = Gene("rs123", "BRCA1", "protein_coding", "Positional")
+    private var functionalGene = Gene("rs123", "FYCO1", "protein_coding", "Functional")
+    private var eQTLGene = Gene("rs123", "ABO", "Liver", "eQTL")
 
     @BeforeEach
     fun setUp(){
@@ -25,13 +25,54 @@ class RankingServiceTest {
         geneServiceField.isAccessible = true
         geneServiceField.set(rankingService, geneService)
 
-        //whenever(geneService.positionalMapping(anyString())).thenReturn(mockGeneMapping(positionalGene))
+        whenever(geneService.positionalMapping(anyString())).thenReturn(positionalGene)
+        whenever(geneService.functionalMapping(anyString())).thenReturn(listOf(functionalGene))
+        whenever(geneService.eQTLMapping(anyString())).thenReturn(listOf(eQTLGene))
 
-        }
     }
 
 
     @Test
-    fun rankGeneMappings() {
+    fun rankGeneMappingsGivesTheMostCommonGene() {
+        whenever(geneService.functionalMapping(anyString())).thenReturn(listOf(Gene("rs123", "BRCA1",
+            "protein_coding", "Functional")))
+        whenever(geneService.eQTLMapping(anyString())).thenReturn(listOf(Gene("rs123", "BRCA1",
+            "Liver", "eQTL")))
+
+
+        val result = rankingService.rankGeneMappings("rs123")
+
+        assertEquals("BRCA1", result)
     }
+
+    @Test
+    fun rankGeneMappingsHasTheMostCommonGeneFromFunctionalAndEQTL() {
+        whenever(geneService.functionalMapping(anyString())).thenReturn(listOf(Gene("rs123", "ABO",
+            "protein_coding", "Functional")))
+
+        val result = rankingService.rankGeneMappings("rs123")
+
+        assertEquals("ABO", result)
+    }
+
+    @Test
+    fun rankGeneMappingsHasTheMostCommonGeneFromPositionalAndEQTL() {
+        whenever(geneService.positionalMapping(anyString())).thenReturn((Gene("rs123", "ABO",
+            "protein_coding", "Positional")))
+
+        val result = rankingService.rankGeneMappings("rs123")
+
+        assertEquals("ABO", result)
+    }
+
+    @Test
+    fun rankGeneMappingsHasTheMostCommonGeneFromPositionalAndFunctional() {
+        whenever(geneService.functionalMapping(anyString())).thenReturn(listOf(Gene("rs123", "BRCA1",
+            "protein_coding", "Functional")))
+
+        val result = rankingService.rankGeneMappings("rs123")
+
+        assertEquals("BRCA1", result)
+    }
+
 }
