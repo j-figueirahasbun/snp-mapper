b48aa9903ef6c4ccf6e5083a6eef8cdabe0c9cf1
j-figueirahasbun <j.figueirahasbun@gmail.com>
2025-01-09 11:38:30 +0100

    Fix changes on front end to remove chromosome visual and change name of the page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import {TextBoxComponent} from "./text-box/text-box.component";
-import {ChromosomeVisualComponent} from "./chromosome-visual/chromosome-visual.component";
+// import {ChromosomeVisualComponent} from "./chromosome-visual/chromosome-visual.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TextBoxComponent, ChromosomeVisualComponent],
+  imports: [RouterOutlet, TextBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
